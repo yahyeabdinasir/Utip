c429449de80897a897c0542a5d312ae26c3f947d
yahyeabdinasirfarah <yahyeabdinasirfarah@gmail.com>
2026-02-01 20:18:54 +0300

    added the reusable widget and remove the merge confilct

--- lib/SliderSplit.dart
+++ lib/SliderSplit.dart
@@ -1,11 +1,10 @@
+ import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-
-class SplitSlider extends StatelessWidget {
-  const SplitSlider({
+class SplitSliderWidget extends StatelessWidget {
+  const SplitSliderWidget({
     super.key,
-    required double tipPercentage, required this.onchange,
-  }) : _tipPercentage = tipPercentage;
+    required double tipPercentage, required this.onchange ,
+  }) : _tipPercentage = tipPercentage ;
 
   final double _tipPercentage;
   final ValueChanged<double> onchange ;
@@ -14,19 +13,20 @@ class SplitSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Slider(
       value: _tipPercentage,
+      onChanged: onchange,
 
       // the variable that we passed triged  the changes of slider
-      onChanged: onchange,
       // onChanged: (value) {
-      //   print("this is the value  {$value}");
-      //   print("this is the tipPercentage  {$_tipPercentage}");
-      //   setState(() {
-      //     _tipPercentage = value;
-      //   });
+
+      // print("this is the value  {$value}");
+      // print("this is the tipPercentage  {$_tipPercentage}");
+      // setState(() {
+      //   _tipPercentage = value;
+      // });
       // },
       min: 0,
       max: 0.5,
-      // divisions: 5,  // the number of the discrete that jums each time it's 5
+      divisions: 5,  // the number of the discrete that jums each time it's 5
       // 5 / 50 = 10 that means the division each time will jumb to the 10
       label: "${(_tipPercentage * 100).round()}%",
     );
