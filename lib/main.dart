fdd432dd9b791bee30b6953171008d4bb3dd9faf
yahyeabdinasirfarah <yahyeabdinasirfarah@gmail.com>
2026-02-01 19:32:58 +0300

    Revert "refactoring the Slider with resusable widget" it prefferable to add the commit that has togather mean the code was same they canot be different commit so let us make the revert
    This reverts commit d817ec1912484d3851359223c23500bfd5b9661a.

--- lib/main.dart
+++ lib/main.dart
@@ -176,11 +176,23 @@ class _TipappState extends State<Tipapp> {
                     // round also make the normal values and removes the digit comes after value .
                     // *100 this makes the int with the digit not between 0 and 1 like 47.39394 so on
                     Text("${(_tipPercentage * 100).round()}%"),
-                    SplitSlider(tipPercentage: _tipPercentage, onchange: (double value) {
-                      setState(() {
-                     _tipPercentage   =  value;
-                      });
-                    },),
+                    Slider(
+                      value: _tipPercentage,
+
+                      // the variable that we passed triged  the changes of slider
+                      onChanged: (value) {
+                        print("this is the value  {$value}");
+                        print("this is the tipPercentage  {$_tipPercentage}");
+                        setState(() {
+                          _tipPercentage = value;
+                        });
+                      },
+                      min: 0,
+                      max: 0.5,
+                      divisions: 5,  // the number of the discrete that jums each time it's 5
+                      // 5 / 50 = 10 that means the division each time will jumb to the 10
+                      label: "${(_tipPercentage * 100).round()}%",
+                    ),
                     // Text("hello"),
                   ],
                 ),
@@ -190,38 +202,6 @@ class _TipappState extends State<Tipapp> {
           // // ),
         ],
       ),
-    );
-  }
-}
-
-class SplitSlider extends StatelessWidget {
-  const SplitSlider({
-    super.key,
-    required double tipPercentage, required this.onchange,
-  }) : _tipPercentage = tipPercentage;
-
-  final double _tipPercentage;
-  final ValueChanged<double> onchange ;
-
-  @override
-  Widget build(BuildContext context) {
-    return Slider(
-      value: _tipPercentage,
-    
-      // the variable that we passed triged  the changes of slider
-      onChanged: onchange,
-      // onChanged: (value) {
-      //   print("this is the value  {$value}");
-      //   print("this is the tipPercentage  {$_tipPercentage}");
-      //   setState(() {
-      //     _tipPercentage = value;
-      //   });
-      // },
-      min: 0,
-      max: 0.5,
-      // divisions: 5,  // the number of the discrete that jums each time it's 5
-      // 5 / 50 = 10 that means the division each time will jumb to the 10
-      label: "${(_tipPercentage * 100).round()}%",
     );
   }
 }
