25ea10d2aa7df89232a23c6d53b202926ee2c529
yahyeabdinasirfarah <yahyeabdinasirfarah@gmail.com>
2026-02-01 16:33:45 +0300

    refactoring the widget to be reusabale

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:utip/personalCounter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,9 +27,29 @@ class Tipapp extends StatefulWidget {
 }
 
 class _TipappState extends State<Tipapp> {
+  //  so here in there if we need to store the value that rebuild screen each time we need to set out here
+  int _personCount = 1;
+  int _countPerPserson = 0;
+
+  void increment() {
+    setState(() {
+      _personCount++;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      if (_personCount > 1){
+        _personCount--;
+      }
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print(context.widget);
+    // storing the theme to the varaible
     var theme = Theme.of(context);
     // copywith it's used to add more the attribute to the existing theme and in order to costomise the default
     // functionaity of the theme
@@ -39,10 +60,12 @@ class _TipappState extends State<Tipapp> {
     );
     return Scaffold(
       appBar: AppBar(
-        // backgroundColor: Colors.red,
-        // title: const Text("app Bar project"),
-        leading: Text("Utip"),
-        centerTitle: true,
+
+        title: Text(
+          "Utip",
+          style: style!.copyWith(color: theme.colorScheme.onPrimaryContainer),
+        ),
+        // centerTitle: true,
       ),
 
       body: Column(
@@ -50,30 +73,108 @@ class _TipappState extends State<Tipapp> {
         // crossAxisAlignment: CrossAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              // color: const Color.fromARGB(255, 160, 128, 216),
-              color: theme.colorScheme.inversePrimary,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              // crossAxisAlignment: CrossAxisAlignment.stretch,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              // padding: EdgeInsets.all(20),
+              margin: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                // color: const Color.fromARGB(255, 160, 128, 216),
+                color: theme.colorScheme.inversePrimary,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.stretch,
 
-              // mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Total per person", style: style?.copyWith()),
-                // Text("\$20", style: theme.textTheme.displayMedium),
-                Text(
-                  "\$20.85",
-                  style: style?.copyWith(
-                    fontSize: theme.textTheme.displaySmall?.fontSize,
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Total per person", style: style?.copyWith()),
+                  // Text("\$20", style: theme.textTheme.displayMedium),
+                  Text(
+                    "$_countPerPserson",
+
+                    style: style?.copyWith(
+                      fontSize: theme.textTheme.displaySmall?.fontSize,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
+          //  added the container that surrneded with the border
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Container(
+              // width: 100,
+              // height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: theme.colorScheme.primary),
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    // Padding(
+                    //   padding: const EdgeInsets.all(8.0),
+                    //   child:
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        //  prefix it's the attribute that it's used to add the prefix of the value
+                        prefixIcon: Icon(Icons.attach_money),
+                        // lable also used as the place holder of the text
+                        label: Text("input the bill"),
+                      ),
+                      // keyboard type it's specify the kind of the input
+                      keyboardType: TextInputType.number,
+                      // onchange we can access the value that we passes the input
+                      // onChanged: (String value) {
+                      //   // print("hello type${value}");
+                      // },
+                    ),
+
+                    // ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            "Split",
+                            style: theme.textTheme.titleMedium,
+                          ),
+                        ),
+                        PersonalCounter(theme: theme, personCount: _personCount, InCrement: increment , Decrement: decrement,),
+                      ],
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            "Tip",
+                            style: TextStyle(color: theme.colorScheme.primary),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text('$_countPerPserson'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    // Text("hello"),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          // // ),
         ],
       ),
     );
---
+++ lib/personalCounter.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+class PersonalCounter extends StatelessWidget {
+  const PersonalCounter({
+    super.key,
+    required this.theme,
+    required int personCount, required this.InCrement, required this.Decrement,
+  }) : _personCount = personCount;
+
+  final ThemeData theme;
+  final int _personCount;
+
+
+  // this void Call back it's same as the () {}'
+  final VoidCallback InCrement;
+  final VoidCallback Decrement;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        IconButton(
+          onPressed:   InCrement,
+          icon: Icon(Icons.add),
+          color: theme.colorScheme.primary,
+        ),
+
+        Text(
+          "$_personCount",
+          style: TextStyle(
+            color: theme.colorScheme.primary,
+          ),
+        ),
+
+        IconButton(
+          onPressed:  Decrement,
+
+          icon: Icon(
+            Icons.remove,
+
+            color: theme.colorScheme.primary,
+          ),
+        ),
+      ],
+    );
+  }
+}
